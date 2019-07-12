2b61ff38333bd4113859dcd1fce2cab827423469
Simeon Andreev <simo1914@abv.bg>
2019-07-13 00:15:52 +0300

    First push

--- Meta Weather/HomeTableViewController.swift
+++ Meta Weather/HomeTableViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeTableViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		self.navigationController?.navigationBar.prefersLargeTitles = true
 	}
 
 
