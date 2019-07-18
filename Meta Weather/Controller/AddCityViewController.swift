5793a6bf930f538f7f1ef3511af2e22b6356f7af
Simeon Andreev <simo1914@abv.bg>
2019-07-19 00:31:19 +0300

    Notification with error for add City functionality

--- Meta Weather/Controller/AddCityViewController.swift
+++ Meta Weather/Controller/AddCityViewController.swift
@@ -8,15 +8,25 @@
 
 import UIKit
 import CoreData
+import NotificationBannerSwift
+
 
 class AddCityViewController: UIViewController {
-	
+	var location: Location?
+	private enum Constant {
+		static let errorTitle = "Error"
+		static let successTitle = "Success"
+		static let newCityMessage = "New city: "
+		static let successfullySavedMessage = "is successfully saved."
+		static let alreadyExistWarrningMessage = "This city already exist, try again with another one."
+		static let doesNotExistMessage = "Does not exist data for that city, try again with another one."
+	}
 	private let persistanceManager: PersistantManager = PersistantManager()
 	
 	@IBOutlet weak var addNewCityTextField: UITextField!
 	override func viewDidLoad() {
         super.viewDidLoad()
-		self.hideKeyboardWhenTappedAround() 
+		self.hideKeyboardWhenTappedAround()
     }
     
 	@IBAction func addNewCity(_ sender: UIButton) {
@@ -24,36 +34,42 @@ class AddCityViewController: UIViewController {
 		if let text = addNewCityTextField.text {
 			ApiService.shared.searchingForExistingCityWeatherForecast(name: text) { [weak self] (response, error) in
 				if let error = error {
-					print("Failed to fetch data:", error)
-					print(error.localizedDescription)
 					self?.removeSpinner()
+					DispatchQueue.main.async {
+					let banner = NotificationBanner(title: Constant.errorTitle, subtitle: error.localizedDescription, style: .warning)
+						banner.show()
+					}
 					return
 				}
 				if let response = response {
 					if response.count > 0 {
-						print(response)
 						DispatchQueue.main.async {
 							if let location = response.first {
 								if let locationExist = self?.persistanceManager.someEntityExists(woeid: location.woeid) {
 									if !locationExist {
 										self?.persistanceManager.save(location: location)
-										print("Save City: \(self?.addNewCityTextField.text ?? "")")
+										let banner = NotificationBanner(title: Constant.successTitle, subtitle: "\(Constant.newCityMessage) \(location.title) \(Constant.successfullySavedMessage)", style: .success)
+										banner.show()
 										self?.removeSpinner()
 										if let navController = self?.navigationController {
 											navController.popViewController(animated: true)
 										}
 									} else {
 										self?.removeSpinner()
+										let banner = NotificationBanner(title: Constant.errorTitle, subtitle: Constant.alreadyExistWarrningMessage, style: .warning)
+										banner.show()
 									}
 								}
 							}
 						}
 					} else {
-						print("Does not exist data for that city, try again with another one.")
 						self?.removeSpinner()
+						DispatchQueue.main.async {
+							let banner = NotificationBanner(title: Constant.errorTitle, subtitle: Constant.doesNotExistMessage, style: .warning)
+							banner.show()
+						}
 					}
 				}
-				
 			}
 		}
 	}
