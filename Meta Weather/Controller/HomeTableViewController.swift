6efb4b80029713c3a99f84d95ea272af253fbb62
Simeon Andreev <simo1914@abv.bg>
2019-07-20 00:01:01 +0300

    Added multiple target selection

--- Meta Weather/Controller/HomeTableViewController.swift
+++ Meta Weather/Controller/HomeTableViewController.swift
@@ -18,6 +18,10 @@ class HomeTableViewController: UITableViewController {
 		static let connectionStatusOfflineTitle = "Offline"
 		static let errorTitle = "Error"
 		static let cantDeleteDefaultLocationMessage = "Can't delete the default location."
+		static let devTargetSelectedMessage = "Dev Target selected"
+		static let uaTargetSelectedMessage = "Ua Target selected"
+		static let productionTargetSelectedMessage = "Production Target selected"
+		static let testTargetSelectedMessage = "Test Target selected"
 		
 		static let tableViewBackgroundColor = UIColor(red: 92/255, green: 140/255, blue: 206/255, alpha: 1.0)
 	}
@@ -32,10 +36,45 @@ class HomeTableViewController: UITableViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		checkTargetSelection()
+		
 		tableView.tableFooterView = UIView()
 		self.navigationController?.navigationBar.prefersLargeTitles = true
 		self.tableView.backgroundColor = Constant.tableViewBackgroundColor
 		
+		checkReachability()
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		for location in defaultCityArray {
+			if !persistanceManager.someEntityExists(woeid: location.woeid) {
+				persistanceManager.save(location: location)
+			}
+		}
+		locationsCD = persistanceManager.loadLocations()
+		self.tableView.reloadData()
+	}
+	
+	private func checkTargetSelection() {
+		#if Dev
+			let banner = StatusBarNotificationBanner(title: Constant.devTargetSelectedMessage, style: .info)
+			banner.show()
+		#elseif Test
+			let banner = StatusBarNotificationBanner(title: Constant.testTargetSelectedMessage, style: .info)
+			banner.show()
+		#elseif Ua
+			let banner = StatusBarNotificationBanner(title: Constant.uaTargetSelectedMessage, style: .info)
+			banner.show()
+		#else
+			let banner = StatusBarNotificationBanner(title: Constant.productionTargetSelectedMessage, style: .info)
+			banner.show()
+		#endif
+	}
+	
+	private func checkReachability() {
 		do {
 			try reachability?.startNotifier()
 		} catch {
@@ -49,18 +88,6 @@ class HomeTableViewController: UITableViewController {
 			let banner = NotificationBanner(title: Constant.connectionStatusTitle, subtitle: Constant.connectionStatusOfflineTitle, style: .danger)
 			banner.show()
 		}
-	}
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		for location in defaultCityArray {
-			if !persistanceManager.someEntityExists(woeid: location.woeid) {
-				persistanceManager.save(location: location)
-			}
-		}
-		locationsCD = persistanceManager.loadLocations()
-		self.tableView.reloadData()
 	}
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
